99fbfde4e4c6715771f138f1cdda9c02c21abb28
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2025-10-29 14:53:24 +0100

    Fix: health

--- src/main.go
+++ src/main.go
@@ -14,7 +14,7 @@ func main() {
   mux := http.NewServeMux()
   mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
-    _, _ = w.Write([]byte("\"status\": \"ok\""))
+    _, _ = w.Write([]byte("\"ok\""))
   })
 
   srv := &http.Server{Addr: ":8080", Handler: mux}
