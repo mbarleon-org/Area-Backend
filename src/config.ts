c7102a1bab95ad58dbcab3a1fd3696a915cb58c7
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2025-11-20 13:19:26 +0100

    Fix: base path

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    BASE_PATH: process.env.BASE_PATH || '/',
+    BASE_PATH: process.env.BASE_PATH || '/api',
 
     LISTEN_ADDRESS: parseInt(process.env.BACKEND_ADDRESS) || 3000
 };
